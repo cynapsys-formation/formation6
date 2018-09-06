79512a4fddde59be7de8948cd404a6f0b4f5a108
hamdi <beligh.hamdi@gmail.Com>
2018-09-06 09:46:38 +0100

    add boostrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// import * as $ from 'jquery';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formation6';
+  isNavbarCollapsed; boolean;
+
 }
